df9730beeee074b33f67b95eb13a14372f8be49d
dorsamoha <dorsamoha@gmail.com>
2020-07-30 15:47:13 +0300

    Updates

---
+++ precache-manifest.cc0e9eb3c3b33181b61c63f8474eadb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81cbb2856a2930773a5c065c8bbfc713",
+    "url": "/samod667/photo-ninja/index.html"
+  },
+  {
+    "revision": "91c1ab9d0065ba2a8b56",
+    "url": "/samod667/photo-ninja/static/css/main.69b19522.chunk.css"
+  },
+  {
+    "revision": "bbc72db424270b5a25cf",
+    "url": "/samod667/photo-ninja/static/js/2.6229bd1e.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/samod667/photo-ninja/static/js/2.6229bd1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c1ab9d0065ba2a8b56",
+    "url": "/samod667/photo-ninja/static/js/main.d8114f08.chunk.js"
+  },
+  {
+    "revision": "6334a70af7f23d98df82",
+    "url": "/samod667/photo-ninja/static/js/runtime-main.6a2d4b8f.js"
+  }
+]);
